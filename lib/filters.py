73551feb022763dd99b09f48c21670cf9b5f2f44
Archan Lagoo <aklagoo@gmail.com>
2021-10-21 14:42:23 -0700

    Completed filter_text_english and filter_text

--- lib/filters.py
+++ lib/filters.py
@@ -3,6 +3,7 @@
 All methods return a boolean.
 """
 from nltk.tokenize import word_tokenize
+from nltk.corpus import words
 from lib import config
 from numpy import ndarray
 from typing import List
@@ -28,5 +29,17 @@ def filter_match_classes(classes: List[str], img_alt: str, img_par: str) -> (boo
 
 
 def filter_text_english(img_alt: str, img_par: str) -> (bool, bool):
-    """Checks if alt-text and par-text contain at least some English words."""
-    return True, True
+    """Checks if alt-text and par-text contain at least some English words.
+
+    This method tokenizes both sentences and checks if there are at least some
+    English words.
+    """
+    dictionary = set(words.words())
+
+    # Count English words
+    alt_words = word_tokenize(img_alt)
+    alt_count = sum([x in dictionary for x in alt_words])
+    par_words = word_tokenize(img_par)
+    par_count = sum([x in dictionary for x in par_words])
+
+    return alt_count >= config.FILTER_TEXT_EN_LEN, par_count >= config.FILTER_TEXT_EN_LEN
--- lib/pipeline.py
+++ lib/pipeline.py
@@ -4,15 +4,39 @@ import torch
 import torchvision
 from torchvision import transforms as transforms
 from lib.detect import _predict
+from lib.filters import filter_text_len, filter_text_english
 
 
-def filter_text(tags: List[dict]) -> List[dict]:
+def filter_text(samples: List[dict]) -> List[dict]:
     """Checks if par and alt are valid. Discards otherwise."""
-    # TODO Fill stub
-    return tags
+    _samples = []
+    for sample in samples:
+        alt = sample['img_alt']
+        par = sample['img_par']
+
+        # Length check
+        alt_len, par_len = filter_text_len(alt, par)
+        if not alt_len and not par_len:
+            continue
+        else:
+            sample['FLAG_ALT_LEN'] = alt_len
+            sample['FLAG_PAR_LEN'] = par_len
+
+        # Language check
+        alt_en, par_en = filter_text_english(alt, par)
+        if not alt_en and not par_en:
+            continue
+        else:
+            sample['FLAG_ALT_EN'] = alt_en
+            sample['FLAG_PAR_EN'] = par_en
+
+        # Append sample to _samples
+        _samples.append(sample)
+
+    return _samples
 
 
-def detect(tags: List[Dict[str, str]]) -> List[Dict[str, str]]:
+def detect(samples: List[Dict[str, str]]) -> List[Dict[str, str]]:
     """Loads and tags images."""
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True)
@@ -22,11 +46,11 @@ def detect(tags: List[Dict[str, str]]) -> List[Dict[str, str]]:
         transforms.ToTensor(),
     ])
 
-    _tags = []
-    for sample in tags:
+    _samples = []
+    for sample in samples:
         # Predict classes and bounding boxes for image
         classes, labels, boxes = _predict(cv2.imread(sample['img_path']), model, device, 0.8, transform)
-        _tags.append({
+        _samples.append({
             'img_uuid': sample['img_uuid'],
             'img_url': sample['img_url'],
             'img_path': sample['img_path'],
@@ -37,7 +61,7 @@ def detect(tags: List[Dict[str, str]]) -> List[Dict[str, str]]:
             'classes': ' '.join(list(set(classes))),
         })
 
-    return _tags
+    return _samples
 
 
 def download(tags: List[dict]) -> List[dict]:
